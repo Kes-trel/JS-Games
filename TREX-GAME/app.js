a6b9445d61d1bbb5c4b470210152e342c400dbae
Kes <74243456+Kes-trel@users.noreply.github.com>
2021-02-23 11:20:34 +0000

    Update app.js

---
+++ TREX-GAME/app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const dino = document.querySelector(".dino")
+
+function control (e) {
+    if (e.keyCode === 32) {
+        //code here on press
+    }
+}
+
+
+
+})
